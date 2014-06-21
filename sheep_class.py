f2dde1cc226d43d64e6482858789a23a2ad6c7ac
Tommy T <tommytham@hotmail.co.uk>
2014-06-21 18:13:20 +0100

    Animals
    1.0

--- sheep_class.py
+++ sheep_class.py
@@ -1,18 +1,18 @@
 from animal_class import *
 
-class Cow(Animal):
-    """A cow """
+class Sheep(Animal):
+    """A sheep"""
     def __init__(self):
-        super().__init__(1,7,7)
-        self._type = "Cow"
+        super().__init__(1,5,9)
+        self._type = "Sheep"
         self._weight = 0
 
     def grow(self,food,water):
         if food >= self._food_need and water >= self._water_need:
             if self._status == "Baby" and water > self._water_need and( food > self._food_need) :
-                self._weight += self._growth_rate * 1.8
+                self._weight += self._growth_rate * 1.7
             elif self._status == "Young" and water > self._water_need and( food > self._food_need):
-                self._weight += self._growth_rate * 1.55
+                self._weight += self._growth_rate * 1.6
             elif self._status == "Old" and water > self._water_need and( food > self._food_need):
                 self._weight += self._growth_rate / 2
             else:
@@ -23,14 +23,14 @@ class Cow(Animal):
 
 def main():
     #create a new potato crop
-    cow_one = Cow()
-    print(cow_one.report())
+    sheep_one = Sheep()
+    print(sheep_one.report())
 
-    manual_grow(cow_one)
-    print(cow_one.report())
+    manual_grow(sheep_one)
+    print(sheep_one.report())
 
-    manual_grow(cow_one)
-    print(cow_one.report())
+    manual_grow(sheep_one)
+    print(sheep_one.report())
 
 if __name__ == "__main__":
     main()
